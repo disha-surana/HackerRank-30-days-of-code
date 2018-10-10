5c659be626ce27c3751d77489cd0c859523546cc
DISHA SURANA <dishasurana11@gmail.com>
2018-10-10 22:02:03 +0530

    Create Day3.cpp

---
+++ Day3.cpp
@@ -0,0 +1,28 @@
+//Day 3: Intro to Conditional Statements
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    if(N % 2!= 0)
+        cout<<"Weird";
+    else
+    {
+        if(N>=2 && N<=5) 
+            cout<<"Not Weird";
+        
+        else if(N>=6 && N<=20)
+            cout<<"Weird";
+        
+        else if(N>20)
+            cout<<"Not Weird";
+            
+    }
+    
+    return 0;
+}
